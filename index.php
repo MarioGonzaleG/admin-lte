b4ec307bcbe35f88377251de41519941b4b7e075
MarioGonzaleG <marioalbertogonzalezgarcia0@gmail.com>
2022-05-21 09:30:02 -0500

    Actualizacion Index y usuarios

--- index.php
+++ index.php
@@ -102,14 +102,38 @@
                     <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Jonathan</a>
+                    <a href="#" class="d-block">Mario</a>
                 </div>
             </div>
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
+                    <!-- usuarios -->
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                          <i class="nav-icon fas fa-users"></i>
+                          <p>
+                            Usuarios
+                            <i class="right fas fa-angle-left"></i>
+                          </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                          <li class="nav-item">
+                            <a href="./views/users/index.html" class="nav-link">
+                              <i class="far fa-circle nav-icon"></i>
+                              <p>Listado</p>
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="./index2.html" class="nav-link">
+                              <i class="far fa-circle nav-icon"></i>
+                              <p>Crear</p>
+                            </a>
+                          </li>
+                        </ul>
+                    </li>
+                    <!-- /usuarios -->
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->
