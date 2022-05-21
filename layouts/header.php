c35eb6d9e00d5ee5d998bdd3c9d001b32cb69f6f
MarioGonzaleG <marioalbertogonzalezgarcia0@gmail.com>
2022-05-21 10:38:24 -0500

    Actualizacion Usuarios

--- layouts/header.php
+++ layouts/header.php
@@ -102,7 +102,7 @@
                     <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Jonathan</a>
+                    <a href="#" class="d-block">Mario</a>
                 </div>
             </div>
 
